9523ffdd6164f852b4760277acd9a7866383dd98
lag47 <lag4777@gmail.com>
2018-01-14 22:26:51 -0500

    removed some unused files

--- equiv.ml
+++ equiv.ml
@@ -61,16 +61,3 @@ let chin_rem pairs =
   match pairs with
   | (a1,n1)::t -> List.fold_left (fun acc x -> acc >>= f x) (Solution (a1,n1)) t
   | [] -> failwith "empty list"
-(*
-let bin_chin a1 a2 n1 n2 =
-  let n1inv= invmod n1 n2 in
-  let x = (n1*n1inv*(a2-a1)+a1) mod (n1*n2)
-  in if x<0 then x + (n1 * n2) else x
-
-let chin_rem ak nk =
-  let pairs = List.combine ak nk in
-  match pairs with
-  | (a1,n1)::t -> List.fold_left
-                    (fun (ai,ni) (ai1,ni1) ->  (bin_chin ai ai1 ni ni1),ni*ni1)
-                    (a1,n1) t
-  | [] -> failwith "Empty List"*)
--- option.mli
+++
@@ -1,7 +0,0 @@
-val return : 'a -> 'a option
-val bind : 'a option -> ('a -> 'b option) -> 'b option
-val map : 'a option -> ('a -> 'b) -> 'b option
-val join : 'a option option -> 'a option
-val extract: 'a -> 'a option -> 'a
-val (>>=) : 'a option -> ('a -> 'b option) -> 'b option
-val (>>|) : 'a option -> ('a -> 'b) -> 'b option
